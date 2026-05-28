0da5136a08979c08530ab8a5310376a4081426e4
Maxorex <2290121@cstj.qc.ca>
2026-05-28 08:17:22 -0400

    depart

--- tp01_dev-web/resources/views/components/footer.blade.php
+++
@@ -1,5 +0,0 @@
-<footer class="footer">
-  <div class="container">
-    <p>&copy; 2026 Blackriver Blades. Tous droits réservés.</p>
-  </div>
-</footer>
--- tp01_dev-web/routes/api.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-use App\Http\Controllers\WeaponController;
-
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
